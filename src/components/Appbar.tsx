73749058ce32a9afc644c18cddd67c4c1dbcbef4
*pknowledge* <mishrasangeeta944@gmail.com>
2024-02-23 18:28:18 +0530

    Navbar

--- src/components/Appbar.tsx
+++ src/components/Appbar.tsx
@@ -1,7 +1,17 @@
 
 function Appbar() {
   return (
-    <div>Appbar</div>
+    <div className=" flex justify-between">
+<div>
+    Youtube
+</div>
+<div>
+Searchbar
+</div>
+<div>
+    Sign in
+</div>
+    </div>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,16 @@ import Image from "next/image";
 import VideoCard from '..//components/VideoCard'
 import { Inter } from "next/font/google";
 import VideoGrid from "@/components/VideoGrid";
+import Appbar from "@/components/Appbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
 <div>
-<VideoGrid>
+  <Appbar/>
+<VideoGrid/>
 
-</VideoGrid>
+
 </div>
   );
 }
